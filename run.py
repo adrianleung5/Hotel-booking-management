406fd0a49ae874d476aa03a43052ed33f5528ab1
adrianleung5 <adrianleung50@gmail.com>
2023-01-02 18:18:56 +0000

    add intake for customer input for name and phone number

--- run.py
+++ run.py
@@ -1,11 +1,37 @@
 import pyfiglet 
 
 class hotel_bill_management:
+    def __init__(self, name = "", phone_number= "", email = "", check_in_date = "", check_out_date = "", no_of_days = 0 , room_price = 0, selected_room = ""):
+        print(pyfiglet.figlet_format("leung hotel"))
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+        self.check_in_date = check_in_date
+        self.check_out_date = check_out_date
+        self.no_of_days = no_of_days
+        self.room_price = room_price
+        self.selected_room = selected_room 
+
     def customer_information (self):
-        name = input("Name")
-        Address = input("address")
-        check_in_date = input("check in ")
-        check_out_date =  input("check out")
+        while True:
+            self.name = input("Enter your name: ")
+            customer_name = self.name 
+            if not (customer_name.replace(" ", "").isalpha() and len(customer_name) > 3) :
+                print("Name can only contain letter and spaces that are greater than three characters")
+                continue
+
+            self.phone_number = input("Enter you phone number: ")
+            if not self.phone_number.isnumeric():
+                print("phone number can only contain numbers")
+            if len(self.phone_number) >= 10:
+                print("Phone number must contain atleast 8 numbers")
+            break
+
+
+
+
+
+
     def calculate_room_price (self): 
         pass
     def calculate_restaurant_expenses (self):
@@ -23,23 +49,23 @@ class hotel_bill_management:
 def main ():
     hotel = hotel_bill_management()
     while True:
-        print(pyfiglet.figlet_format("leung hotel"))
-        print("Welcome to the hotel please select from the options below\n")
-        print("1 enter customer information\n")
-        print("2 calculate the room price\n")
-        print("3 calculate restaurant expenses\n")
-        print("4 calculate total\n") 
-        print("5 generete bill\n")
-        print("6 retrieve bill for room\n")
-        print("7 remove bill for room\n")
-        print("8 exit\n")
+      
+        print("Welcome to the hotel please select from the options below")
+        print("1 enter customer information")
+        print("2 calculate the room price")
+        print("3 calculate restaurant expenses")
+        print("4 calculate total") 
+        print("5 generete bill")
+        print("6 retrieve bill for room")
+        print("7 remove bill for room")
+        print("8 exit")
         choice = input("please enter your choice\n")
         if choice == "1":
             hotel.customer_information()
         elif choice == "2":
             hotel.calculate_room_price()
         elif choice == "3":
-            hotel.calculate_room_price()
+            hotel.calculate_restaurant_expenses()
         elif choice == "4":
             hotel.calculate_total()
         elif choice == "5":
@@ -49,7 +75,7 @@ def main ():
         elif choice == "7":
             hotel.remove_bill_for_room()
         elif choice == "8":
-            quit ()
+            quit()
         else:
             print("please enter the correct choice\n")
 
