d309827f38914d3df64676c6da9e1df4f2d4db80
adrianleung5 <adrianleung50@gmail.com>
2023-01-02 19:26:17 +0000

    add check in and check out out information for customers

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 import pyfiglet 
+import datetime
+
+red_color = '\033[91m'
+green_color  = '\33[32m'
+
+color_end = '\033[0m'
+
+
 
 class hotel_bill_management:
     def __init__(self, name = "", phone_number= "", email = "", check_in_date = "", check_out_date = "", no_of_days = 0 , room_price = 0, selected_room = ""):
@@ -17,20 +25,32 @@ class hotel_bill_management:
             self.name = input("Enter your name: ")
             customer_name = self.name 
             if not (customer_name.replace(" ", "").isalpha() and len(customer_name) > 3) :
-                print("Name can only contain letter and spaces that are greater than three characters")
+                print(red_color+"Name can only contain letter and spaces that are greater than three characters"+color_end)
                 continue
 
             self.phone_number = input("Enter you phone number: ")
             if not self.phone_number.isnumeric():
-                print("phone number can only contain numbers")
-            if len(self.phone_number) >= 10:
-                print("Phone number must contain atleast 8 numbers")
-            break
+                print(red_color+"phone number can only contain numbers"+color_end)
+                continue
+            if len(self.phone_number) < 10:
+                print(red_color+"Phone number must contain atleast 10 numbers"+color_end)
+                continue
 
+            self.email = input("Enter your email")
+            # add in validation for email
 
-
-
-
+            self.check_in_date = input("enter your check in date in the format of dd/mm/yyyy : ")
+            if not self.isvalidcheck_in_date(self.check_in_date):
+                print(red_color+"Check in date is not valid or it is in the past, please try again"+color_end)
+                continue
+            
+            self.check_out_date = input("enter your check out date in the format of dd/mm/yyyy : ")
+            if not self.isvalidcheck_out_date(self.check_in_date, self.check_out_date):
+                print(red_color+"The checkout date is not valid, please try again" + color_end) 
+                continue
+            else:
+                print(green_color+"\n***** Customer Information added Sucessfully ***** \n"+color_end)
+                print(green_color+ "Customer wishes to stay for " + str(self.no_of_days)+" days, please choose rooms next\n"+color_end)
 
     def calculate_room_price (self): 
         pass
@@ -44,6 +64,39 @@ class hotel_bill_management:
         pass
     def remove_bill_for_room (self):
         pass
+
+    def isvalidcheck_in_date(self, input_date) :
+        valid_date = True
+        try:
+            day, month, year = input_date.split("/")
+            check_in_date = datetime.datetime(int(year),int(month),int(day))
+            present_date = datetime.datetime.now()
+            if check_in_date.date() < present_date.date():
+                valid_date = False
+        except ValueError:
+            valid_date = False
+        finally:
+            return valid_date
+
+    def isvalidcheck_out_date(self,check_in_date,checkout_date):
+        valid_date = True
+        try:
+            day1, month1, year1 = check_in_date.split("/")
+            day2, month2, year2 = checkout_date.split("/")
+
+            checkin_date1 = datetime.datetime(int(year1),int(month1),int(day1))
+            checkout_date1 = datetime.datetime(int(year2),int(month2),int(day2))
+            
+            if checkin_date1.date() >= checkout_date1.date():
+                print(red_color+"Check out date cannot be before check in date, please try again" +color_end)
+                valid_date = False
+            else:
+                self.no_of_days = (checkout_date1.date() - checkin_date1.date()).days
+        except ValueError:
+            valid_date = False
+        finally:
+            return valid_date
+
 
 
 def main ():
